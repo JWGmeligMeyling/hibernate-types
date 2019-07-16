e64145fbc1730cf61dd6fa59e06fa5e81319973c
Jan-Willem Gmelig Meyling <jan-willem@youngmediaexperts.nl>
2019-07-16 15:25:44 +0200

    Alternative PostgreSQLEnumType constructor

--- hibernate-types-52/src/main/java/com/vladmihalcea/hibernate/type/basic/PostgreSQLEnumType.java
+++ hibernate-types-52/src/main/java/com/vladmihalcea/hibernate/type/basic/PostgreSQLEnumType.java
@@ -3,10 +3,12 @@ package com.vladmihalcea.hibernate.type.basic;
 import com.vladmihalcea.hibernate.type.util.Configuration;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.type.EnumType;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Properties;
 
 /**
  * Maps an {@link Enum} to a PostgreSQL ENUM column type.
@@ -26,6 +28,14 @@ public class PostgreSQLEnumType extends org.hibernate.type.EnumType {
      */
     public PostgreSQLEnumType() {
         this.configuration = Configuration.INSTANCE;
+    }
+
+    public PostgreSQLEnumType(Class<? extends Enum<?>> klass) {
+        this();
+        Properties properties = new Properties();
+        properties.setProperty(EnumType.ENUM, klass.getName());
+        properties.setProperty(EnumType.NAMED, Boolean.TRUE.toString());
+        setParameterValues(properties);
     }
 
     /**
